24e32358f97f82d3c79778c35ef8589835219b92
a13dd12371 <108872467+a13dd12371@users.noreply.github.com>
2022-08-22 17:58:03 +0300

    curl.sh

---
+++ curl.sh
@@ -0,0 +1,14 @@
+# curl.sh
+echo "-------------------"
+echo "http://localhost:8080"
+echo ""
+curl http://localhost:8080
+echo ""
+echo ""
+
+echo "-------------------"
+echo "-k https://localhost:8080"
+echo ""
+curl -k https://localhost:8080
+echo ""
+echo "
